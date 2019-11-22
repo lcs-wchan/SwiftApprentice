43b7db9c402a25b53516c0eccc7034e392e1ec14
WilliamChan <wchan@lcs.on.ca>
2019-11-22 14:33:33 -0500

    Initial Commit

---
+++ SwiftApprentice/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftApprentice
+//
+//  Created by William Chan on 2019-11-22.
+//  Copyright © 2019 WilliamChan. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
